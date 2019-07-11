0bc382b19666c89003b8292489a5c9e4d164cddd
music635 <angeljhsperegrino@gmail.com>
2019-07-11 13:25:32 -0500

    gson
    Se comentan las instancias de las clases Gson y Empleados y se crea una variable Json a la que se le asignan los valores de  la clase Empleado

--- app/src/main/java/com/example/gson/MainActivity.java
+++ app/src/main/java/com/example/gson/MainActivity.java
@@ -16,11 +16,15 @@ public class MainActivity extends AppCompatActivity {
             //Se instancia la clase Gson
             Gson gson = new Gson();
 
+            /*
             //Se instancia la clase Empleado
             Empleado employee = new Empleado("Alberto", 23, "angeljhsperegrino@gmail.com");
 
             //Se convierte el Gson en Json
             String json = gson.toJson(employee);
+            */
+
+            String json = "{\"firstName\" : \"Alberto\", \"age\" :30, \"email\" : \"angeljhsperegrino@gmail.com\"}";
 
 
 
