a271b7909a12cf8250841272564f5e9da4321bae
music635 <angeljhsperegrino@gmail.com>
2019-07-11 12:10:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gson/Empleado.java
@@ -0,0 +1,14 @@
+package com.example.gson;
+
+public class Empleado {
+
+    private String firstName;
+    private int age;
+    private String email;
+
+    public Empleado(String firstName, int age, String email) {
+        this.firstName = firstName;
+        this.age = age;
+        this.email = email;
+    }
+}
