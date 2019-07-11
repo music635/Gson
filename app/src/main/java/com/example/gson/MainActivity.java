08f3fd5abd05948b3b9bf31efc33c73889f15917
music635 <angeljhsperegrino@gmail.com>
2019-07-11 12:18:01 -0500

    Se crea la instancia para la clase Gson

--- app/src/main/java/com/example/gson/MainActivity.java
+++ app/src/main/java/com/example/gson/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            Gson gson = new Gson();
 
 
     }
