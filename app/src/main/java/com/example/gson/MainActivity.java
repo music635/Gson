c22055223b7cd7a2267ce7f920f2edf26b7e4a59
music635 <angeljhsperegrino@gmail.com>
2019-07-12 11:32:05 -0500

    Volviendo a la declaración del Json
    Aqui se vuleva a la declaración del json que habiamos comentado y se vuelve a comentar las instanciaciones antes realizadas

--- app/src/main/java/com/example/gson/MainActivity.java
+++ app/src/main/java/com/example/gson/MainActivity.java
@@ -17,14 +17,19 @@ public class MainActivity extends AppCompatActivity {
             Gson gson = new Gson();
 
             /*
+            //Instanciación de la clase Adress y asignación a sus atributos
+            Adress adress = new Adress("Mexico", "Villahermosa");
+
+
             //Se instancia la clase Empleado
-            Empleado employee = new Empleado("Alberto", 23, "angeljhsperegrino@gmail.com");
+            Empleado employee = new Empleado("Alberto", 23, "angeljhsperegrino@gmail.com", adress);
 
             //Se convierte el Gson en Json
             String json = gson.toJson(employee);
             */
 
-            String json = "{\"first_name\" : \"Alberto\", \"age\" :30, \"email\" : \"angeljhsperegrino@gmail.com\"}";
+
+            String json = "{\"adress\" :{ \"city\" : \"Villahermosa\" , \"country\" : \"Mexico\"}, \"first_name\" : \"Alberto\", \"age\" :30, \"email\" : \"angeljhsperegrino@gmail.com\"}";
 
             //se instancia la clase Empleado y se transforma a Json
             Empleado empleado = gson.fromJson(json, Empleado.class);
